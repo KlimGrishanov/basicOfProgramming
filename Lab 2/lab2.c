2c5d5b8c422439d825ec783fd57704a96d849a0e
klimgrishanov13 <klim.grishanov@mail.ru>
2022-09-23 11:07:26 +0300

    Lab #2: Refactoring New function: checkIsNan and printCell.

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -5,11 +5,13 @@ void funcFor(double fromNum, double stepI, int iter);
 void funcWhile(double fromNum, double toNum, double stepI);
 void funcDoWhile(double fromNum, double toNum, double stepI);
 
-double f(double i);
 void start(double fromNum, double toNum, int iter);
 void input(double *fromNum, double *toNum, int *iter);
-int check(double fromNum, double toNum, int iter);
+void checkIsNan(double value);
+void printCell(double value);
 
+int check(double fromNum, double toNum, int iter);
+double f(double i);
 double stepIter(double fromNum, double toNum, int iter);
 
 int main(void){
@@ -26,6 +28,18 @@ int main(void){
         }
     }
     return 0;
+}
+
+void printCell(double value){
+    printf(" %6.5lf |", value);
+}
+
+void checkIsNan(double value){
+    if(isnan(value)){
+        printf(" CantCalc|");
+    }else{
+        printCell(value);
+    }
 }
 
 double f(double i){
@@ -46,16 +60,13 @@ int check(double fromNum, double toNum, int iter){
     if(iter <= 0){
         printf("Iter below 0\n");
         ans = 0;
-    }
-    else if(fromNum == toNum && iter != 1){
+    }else if(fromNum == toNum && iter != 1){
         printf("Right border equal left border and iter more than 1\n");
         ans = 0;
-    }
-    else if(fromNum != toNum && iter == 1){
+    }else if(fromNum != toNum && iter == 1){
         printf("Iter equal 1, but must be at least 2 if right border dont equal left border\n");
         ans = 0;
-    }
-    else{
+    }else{
         ans = 1;
     }
     return ans;
@@ -81,48 +92,37 @@ void funcFor(double fromNum, double stepI, int iter){
     double x = fromNum;
     printf("for:\nx    |");
     for(int i = 0; i < iter; i++){
-        printf(" %6.5lf |", fromNum);
+        printCell(fromNum);
         fromNum += stepI;
     }
     fromNum = x;
     printf("\nf(x) |");
     for(int i = 0; i < iter; i++){
-        double temp = f(fromNum);
-        if(isnan(temp)){
-            printf(" Cant calc |");
-        }else{
-            printf(" %6.5lf |", f(fromNum));
-        }
+        checkIsNan(f(fromNum));
         fromNum += stepI;
     }
+}
+
+void leftEqualRightBorder(double value){
+    printCell(value);
+    printf("\nf(x) |");
+    checkIsNan(f(value));
 }
 
 void funcWhile(double fromNum, double toNum, double stepI){
     printf("\nWhile:\nx    |");
     double temp = fromNum;
-    double check = f(fromNum);
     if(fromNum == toNum){
-        printf(" %6.5lf |", fromNum);
-        printf("\nf(x) |");
-        if(isnan(check)){
-            printf(" Cant calc |");
-        }else {
-            printf(" %6.5lf |", f(fromNum));
-        }
+        leftEqualRightBorder(fromNum);
     }else{
         while(fromNum < toNum + stepI){
-            printf(" %6.5lf |", fromNum);
+            printCell(fromNum);
             fromNum += stepI;
         }
         fromNum = temp;
         printf("\nf(x) |");
         while(fromNum < toNum + stepI){
-            check = f(fromNum);
-            if(isnan((check))){
-                printf(" Cant calc |");
-            }else{
-                printf(" %6.5lf |", f(fromNum));
-            }
+            checkIsNan(f(fromNum));
             fromNum += stepI;
         }
     }
@@ -132,18 +132,13 @@ void funcDoWhile(double fromNum, double toNum, double stepI){
     printf("\nDo While:\nx    |");
     double temp = fromNum;
     do{
-        printf(" %6.5lf |", fromNum);
+        printCell(fromNum);
         fromNum += stepI;
     }while(fromNum < toNum + stepI);
     fromNum = temp;
     printf("\nf(x) |");
     do{
-        double check = f(fromNum);
-        if(isnan((check))){
-            printf(" Cant calc |");
-        }else{
-            printf(" %6.5lf |", f(fromNum));
-        }
+        checkIsNan(f(fromNum));
         fromNum += stepI;
     }while(fromNum < toNum + stepI);
 }
